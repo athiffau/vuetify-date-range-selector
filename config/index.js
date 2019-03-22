a108d6913e3964ec32d403ba8518d8d58f983919
Alain Thiffault <athiffau@effectivemomentum.com>
2019-03-22 13:54:56 -0400

    Revert "Github pages in root"
    This reverts commit 351bdaf1fe0566a948cf91d65212eba4d73dbc05.

--- config/index.js
+++ config/index.js
@@ -50,12 +50,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
